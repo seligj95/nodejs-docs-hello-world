4db6b0def880b5f9602b9093ea8141a1857403e7
Jordan Selig <11475594+seligj95@users.noreply.github.com>
2021-11-10 21:27:15 +0000

    update for v3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello from v2!");
+    response.end("Hello from v3!");
 });
 
 const port = process.env.PORT || 1337;
