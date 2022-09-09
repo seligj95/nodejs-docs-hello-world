ecaaa74f951b9f205aa97cc77d2a57fbf8563e21
Jordan Selig <11475594+seligj95@users.noreply.github.com>
2022-09-09 10:17:57 -0400

    update text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello from v2!");
+    response.end("Hello from App Service Environment v3 inside the Contoso virtual netowrk!");
 });
 
 const port = process.env.PORT || 1337;
