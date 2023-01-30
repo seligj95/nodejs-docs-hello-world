8754345325a1055381853138f33c4dece1716f38
Jordan Selig <11475594+seligj95@users.noreply.github.com>
2023-01-30 13:36:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const region = process.env.region
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello from App Service Environment v3 inside the Contoso virtual netowrk!");
+    response.end("Hello from ${East US}!");
 });
 
 const port = process.env.PORT || 1337;
